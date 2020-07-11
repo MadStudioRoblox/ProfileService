e3a0e56f8234bcf0347b481579f99a67c568513c
Laimonas Mileska <64644822+LM-loleris@users.noreply.github.com>
2020-07-11 22:20:13 +0300

    Create PlayerProfileExample.lua

---
+++ PlayerProfileExample.lua
@@ -0,0 +1,59 @@
+local ProfileTemplate = {
+	Cash = 0,
+	Items = {},
+	LogInTimes = 0,
+}
+
+----- Loaded Services & Modules -----
+
+local ProfileService = require(game.ServerScriptService.ProfileService)
+
+----- Private Variables -----
+
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+
+local GameProfileStore = ProfileService.GetProfileStore("PlayerData", ProfileTemplate)
+
+local Profiles = {} -- [player] = profile
+
+----- Private Functions -----
+
+local function DoSomethingWithALoadedProfile(player, profile)
+	profile.Data.LogInTimes = profile.Data.LogInTimes + 1
+	print(player.Name .. " has logged in " .. tostring(profile.Data.LogInTimes) .. " time" .. ((profile.Data.LogInTimes > 1) and "s" or ""))
+end
+
+local function PlayerAdded(player)
+	local profile = GameProfileStore:LoadProfileAsync("Player_" .. player.UserId, "ForceLoad")
+	if profile ~= nil then
+		profile:ListenToRelease(function()
+			Profiles[player] = nil
+			player:Kick() -- The profile could've been loaded on another Roblox server
+		end)
+		if player:IsDescendantOf(Players) == true then
+			Profiles[player] = profile
+			-- A profile has been successfully loaded!
+			DoSomethingWithALoadedProfile(player, profile)
+		else
+			profile:Release() -- Player left before the profile loaded
+		end
+	end
+end
+
+----- Initialize -----
+
+for _, player in ipairs(Players:GetPlayers()) do
+	coroutine.wrap(PlayerAdded)(player)
+end
+
+----- Connections -----
+
+Players.PlayerAdded:Connect(PlayerAdded)
+
+Players.PlayerRemoving:Connect(function(player)
+	local profile = Profiles[player]
+	if profile ~= nil then
+		profile:Release()
+	end
+end)
